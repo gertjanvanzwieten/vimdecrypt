02056cb666c598403b0f3783b76528e2fb1aed11
Gertjan van Zwieten <git@gjvz.nl>
2014-12-02 21:37:45 +0100

    fix for python3

--- vimdecrypt.py
+++ vimdecrypt.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import sys, os, getpass, cffi
 
 ffi = cffi.FFI()
@@ -11,20 +12,20 @@ ffi.cdef( '''
 blowfish = ffi.dlopen( os.path.join( os.path.dirname(__file__), 'blowfish.so' ) )
 
 def decrypt( filename ):
-  with open( filename ) as data:
-    assert data.read(12) == 'VimCrypt~02!', 'input should be a vim-encrypted file'
+  with open( filename, 'rb' ) as data:
+    assert data.read(12) == b'VimCrypt~02!', 'input should be a vim-encrypted file'
     salt = data.read(8)
     seed = data.read(8)
     assert len(salt) == len(seed) == 8, 'data ended prematurely'
     buf = ffi.new( "unsigned char[]", data.read() )
   pw = getpass.getpass( 'password: ' )
   assert pw, 'empty password'
-  blowfish.bf_key_init( pw, salt, len(salt) )
+  blowfish.bf_key_init( pw.encode(), salt, len(salt) )
   blowfish.bf_cfb_init( seed, len(seed) )
   blowfish.bf_crypt_decode( buf, len(buf)-1 );
-  return ffi.string( buf )
+  return ffi.string( buf ).decode()
 
 if __name__ == '__main__':
   if len(sys.argv) != 2:
     sys.exit( 'usage: %s [filename]' % os.path.basename(__file__) )
-  print decrypt( sys.argv[1] )
+  print( decrypt( sys.argv[1] ) )

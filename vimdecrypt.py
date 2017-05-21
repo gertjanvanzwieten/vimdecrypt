8e9babc51a8c30076e4a935f155146ef520339a7
Gertjan van Zwieten <gertjanvanzwieten@evalf.nl>
2017-05-21 15:23:03 +0200

    change magics tuple to method_nr dict

--- vimdecrypt.py
+++ vimdecrypt.py
@@ -8,15 +8,15 @@ class cryptstate_T( ctypes.Structure ):
   _fields_ = ('method_nr', ctypes.c_int), ('method_state', ctypes.c_void_p)
 
 def decrypt( filename ):
-  magics = None, b'VimCrypt~02!', b'VimCrypt~03!'
+  method_nr = { b'VimCrypt~02!':1, b'VimCrypt~03!':2 }
   with open( os.path.expanduser(filename), 'rb' ) as data:
     magic = data.read(12)
-    assert magic in magics, 'input should be a vim-encrypted file'
+    assert magic in method_nr, 'input should be a vim-encrypted file'
     salt = data.read(8)
     seed = data.read(8)
     assert len(salt) == len(seed) == 8, 'data ended prematurely'
     buf = ctypes.create_string_buffer( data.read() )
-  state = cryptstate_T( magics.index(magic) )
+  state = cryptstate_T( method_nr[magic] )
   if state.method_nr == 1:
     warnings.warn( 'file uses weak encryption' )
   pw = getpass.getpass( 'password: ' )

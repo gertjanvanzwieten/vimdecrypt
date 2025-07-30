c14a5fc56f4d5ce82d50c475f9dd2ae04ac725cd
Carl-Erik Kopseng <carlerik@gmail.com>
2025-07-30 13:40:36 +0200

    Namespace modules

--- src/vimdecrypt/cli.py
+++ src/vimdecrypt/cli.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import sys
-import vimdecrypt
+from .vimdecrypt import decrypt
 
 def main():
     if len(sys.argv) > 2:
         sys.exit('usage: vimdecrypt [path]')
 
-    print(vimdecrypt.decrypt(sys.argv[1] if len(sys.argv) == 2 else sys.stdin.buffer))
+    file = sys.argv[1] if len(sys.argv) == 2 else sys.stdin.buffer
+    print(decrypt(file))
 
 if __name__ == "__main__":
     main()
